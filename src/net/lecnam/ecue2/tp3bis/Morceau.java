58932efe5b1a76c3520e7e1933c87fdb21e40142
Stephane Bance <bancestephane.r@gmail.com>
2026-09-21 12:56:30 -0200

    Exercice 3 Fini

--- src/net/lecnam/ecue2/tp3bis/Morceau.java
+++ src/net/lecnam/ecue2/tp3bis/Morceau.java
@@ -4,5 +4,15 @@ public class Morceau {
     String titre;
     String artist;
     Duree duree;
-    int nEcoute;
+    int nEcoute = 0;
+
+    public Morceau(String titre, String artist, Duree duree) {
+        this.titre = titre;
+        this.artist = artist;
+        this.duree = duree;
+    }
+
+    public Morceau(String titre, String artist, int minutes, int secondes) {
+        this(titre, artist, new Duree(minutes, secondes));
+    }
 }
