b44340e74ae5ad753647bd5d7a921794a7aa4cea
Stephane Bance <bancestephane.r@gmail.com>
2026-09-22 04:48:37 -0200

    Exercice 10 Fini

--- src/net/lecnam/ecue2/tp3bis/Exec.java
+++ src/net/lecnam/ecue2/tp3bis/Exec.java
@@ -1,24 +1,55 @@
 package net.lecnam.ecue2.tp3bis;
 
 
+import java.util.Random;
+
 public class Exec  {
 
     public static void main(String[] args) {
-        Duree d1 = new Duree(360);
-        Duree d2 = new Duree(3, 59);
-        Duree d3 = new Duree(45, 345);
+        Playlist pl = new Playlist("Hit of 1500-to-2999");
+        Random rand = new Random();
 
-        System.out.println("d1 : " + d1);
-        System.out.println("d1 en secondes : " + d1.enSeconde());
-        System.out.println("d2 : " + d2);
-        System.out.println("d2 en secondes : " + d2.enSeconde());
-        System.out.println("d3 : " + d3);
-        System.out.println("d3 en secondes : " + d3.enSeconde());
+        // Morceaux
+        Morceau m1 = new Morceau("Long long run", "Arthur Grant", 10, 45);
+        Morceau m2 = new Morceau("Bip bip", "Arthur Grant", 0, 45);
+        Morceau m3 = new Morceau("Mes emmerdes", "Charles Aznavour", 3, 7);
+        Morceau m4 = new Morceau("Deux Guitare", "Charles Aznavour", 3, 51);
+        Morceau m5 = new Morceau("Hier encore", "Charles Aznavour", 2, 23);
+        Morceau m6 = new Morceau("Menilmontant", "Charles Trenet", 3, 20);
+        Morceau m7 = new Morceau("I will survive", "Gloria Gaynor", 3, 15);
+        Morceau m8 = new Morceau("Imposter syndrome", "Sydney Gish", 4, 49);
+        Morceau m9 = new Morceau("La foule", "Edith Piaf", 3, 23);
+        Morceau m10 = new Morceau("Tout doucement", "Bibie", 4, 12);
 
-        d1.ajouter(d2);
-        System.out.println("d1 + d2 : " + d1);
+        pl.ajout(m1);
+        pl.ajout(m2);
+        pl.ajout(m3);
+        pl.ajout(m4);
+        pl.ajout(m5);
+        pl.ajout(m6);
+        pl.ajout(m7);
+        pl.ajout(m8);
+        pl.ajout(m9);
 
-        System.out.println("d3 > d2 : " + d3.estPlusLongueQue(d2));
+        if (pl.ajout(m10)) {
+            System.out.println("Nouvelle Ajout");
+        }
+        if (!(pl.ajout(m4))) {
+            System.out.println("Déjà là !");
+        };
+
+
+        System.out.println(pl.retourneDureeTotale());
+
+        for (int i = 0; i <= 200;i++) {
+            pl.morceaux[rand.nextInt(10)].ecouter();
+        }
+
+        pl.ecouterTout();
+
+        System.out.println("Le plus long : " + pl.retourneMorceauLePlusLong());
+        System.out.println("Le plus écouter : " + pl.retourneMorceauLePlusEcoute());
+
+        System.out.println("Aznavour à " + pl.retourneNbMorceauxDe("Charles Aznavour") + " morceaux dans la playlist.");
     }
-
 }
